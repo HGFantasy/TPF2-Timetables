781a46730c7f822f071f5a694f79bf3fe2a3de19
HGFantasy <thegazelletv@gmail.com>
2025-12-25 18:34:14 +0100

    Remove unused timetable helper tests from main test suite

--- tests/main_tests.lua
+++ tests/main_tests.lua
@@ -1,9 +1,5 @@
 local timetableTests = require "tests.timetable_tests"
-local timetableHelperTests = require "tests.timetable_helper_tests"
 
 
 print("running timetable tests")
 timetableTests.test()
-
-print("running timetable helper tests")
-timetableHelperTests.test()
